8ed2c9edd2289986a99af252010f9e4bb5eae40b
Atharva1763 <atharva1763@gmail.com>
2022-07-27 20:07:20 +0530

    Added Merge sort

--- Sorting_Algorithm.c
+++ Sorting_Algorithm.c
@@ -2,9 +2,12 @@
 
 //--------------Quick Sort-----------------//
 void swap(int *a, int *b);
-int partition(int array[], int low, int high);
-void quickSort(int array[], int low, int high);
+int partition(int array[], int lo, int hi);
+void quickSort(int array[], int lo, int hi);
 
+//--------------Merge Sort-----------------//
+void merge(int arr[], int l, int m, int r);
+void mergeSort(int arr[], int l, int r);
 
 int main()
 {
@@ -22,7 +25,8 @@ int main()
 
     printf("\nEnter the number corresponding to the opperation that you want to perform\n");
     printf("Enter 1: Quick Sort\n");
-    
+    printf("Enter 2: Merge Sort\n");
+
     int operation;
     scanf( "%d" , &operation );
     
@@ -30,6 +34,9 @@ int main()
     {
         case 1:
             quickSort(arr, 0, n - 1);
+            break;
+        case 2:
+            mergeSort(arr, 0, n - 1);
             break;
     }
     
@@ -42,6 +49,10 @@ int main()
     printf("\n");
 }
 
+//-----------------------------------------//
+//--------------Quick Sort-----------------//
+//-----------------------------------------//
+
 void swap(int *a, int *b)         // function to swap elements
 {
     int t = *a;
@@ -49,16 +60,15 @@ void swap(int *a, int *b)         // function to swap elements
     *b = t;
 }
 
-int partition(int array[], int low, int high)        // function to find the partition position
+int partition(int array[], int lo, int hi)        // function to find the partition position
 {
-
     // selecting the rightmost element as pivot
-    int pivot = array[high];
+    int pivot = array[hi];
 
     // pointer for greater element
-    int i = (low - 1);
+    int i = (lo - 1);
 
-    for (int j = low; j < high; j++)
+    for (int j = lo; j < hi; j++)
     {
         if (array[j] <= pivot)
         {
@@ -70,22 +80,91 @@ int partition(int array[], int low, int high)        // function to find the par
         }
     }
 
-    swap(&array[i + 1], &array[high]);
+    swap(&array[i + 1], &array[hi]);
 
     // return the partition point
     return (i + 1);
 }
 
-void quickSort(int array[], int low, int high)
+void quickSort(int array[], int lo, int hi)
 {
-    if (low < high)
+    if (lo < hi)
     {
         // find the pivot element such that elements smaller than pivot are on left of pivot and elements greater than pivot are on right of pivot
 
-        int pi = partition(array, low, high);
+        int pi = partition(array, lo, hi);
 
-        quickSort(array, low, pi - 1);
+        quickSort(array, lo, pi - 1);
 
-        quickSort(array, pi + 1, high);
+        quickSort(array, pi + 1, hi);
     }
 }
+
+//-----------------------------------------//
+//--------------Merge Sort-----------------//
+//-----------------------------------------//
+
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int Left[n1], Right[n2];
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		Left[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		Right[j] = arr[m + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2) {
+		if (Left[i] <= Right[j]) {
+			arr[k] = Left[i];
+			i++;
+		}
+		else {
+			arr[k] = Right[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while (i < n1) {                           //Copy the remaining elements of L[], if there are any
+		arr[k] = Left[i];
+		i++;
+		k++;
+	}
+	
+	while (j < n2) {                           // Copy the remaining elements of R[], if there are any 
+		arr[k] = Right[j];
+		j++;
+		k++;
+	}
+}
+
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r) {
+		// Same as (l+r)/2, but avoids overflo for
+		// large l and h
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
+
