0cf15f701f6111280e4531fe61ce6b7742605d10
Atharva1763 <atharva1763@gmail.com>
2022-07-27 20:30:30 +0530

    Added Selection sort

--- Sorting_Algorithm.c
+++ Sorting_Algorithm.c
@@ -13,6 +13,11 @@ void mergeSort(int arr[], int l, int r);
 //--------------Bubble Sort-----------------//
 void bubbleSort(int arr[], int n);
 
+//--------------Selection Sort-----------------//
+void selectionsort(int array[], int n);
+
+
+
 int main()
 {
     int n;
@@ -31,6 +36,7 @@ int main()
     printf("Enter 1: Quick Sort\n");
     printf("Enter 2: Merge Sort\n");
     printf("Enter 3: Bubble Sort\n");
+    printf("Enter 4: Selection Sort\n");
 
     int operation;
     scanf( "%d" , &operation );
@@ -45,6 +51,10 @@ int main()
             break;
         case 3:
             bubbleSort(arr, n);
+            break;
+        case 4:
+            selectionsort(arr,n);
+            break;
     }
     
     printf("Sorted array in ascending order: \n");
@@ -175,9 +185,9 @@ void mergeSort(int arr[], int l, int r)
 	}
 }
 
-//-----------------------------------------//
+//------------------------------------------//
 //--------------Bubble Sort-----------------//
-//-----------------------------------------//
+//------------------------------------------//
 
 void bubbleSort(int arr[], int n)
 {
@@ -190,3 +200,20 @@ void bubbleSort(int arr[], int n)
                 swap(&arr[j], &arr[j + 1]);
 }
 
+//---------------------------------------------//
+//--------------Selection Sort-----------------//
+//---------------------------------------------//
+
+void selectionsort(int array[], int n){
+  for (int i = 0; i < n - 1; i++) {
+    int index_of_min = i;
+    for (int j = i + 1; j < n; j++) {
+      // Select the minimum element in each loop.
+      if (array[j] < array[index_of_min])
+        index_of_min = j;
+    }
+
+    // put min at the correct position
+    swap(&array[index_of_min], &array[i]);
+  }
+}
